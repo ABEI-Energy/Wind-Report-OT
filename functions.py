b2565033cc81a1234f5c12403aa74effb686ca9e
Gonzalo dC <116873281+Gorgonzzalo@users.noreply.github.com>
2023-11-22 11:10:17 +0100

    pre-change

---
+++ functions.py
@@ -0,0 +1,5 @@
+'''
+
+This script contains the functions to be called from streamlit.py
+
+'''
---
+++ streamlit.py
@@ -0,0 +1,4 @@
+'''
+This script serves as the holder of the streamlit webpage. All the main functions that deal with the documentation and such, will be held in another scripts
+
+'''
